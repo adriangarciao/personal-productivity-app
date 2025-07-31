05b4c300c35c96e180c1cd8cdf7c61af1ae268ba
Adrian Garcia <garcia.adriann08@gmail.com>
2025-07-30 20:06:59 -0500

    Initial Commit

---
+++ src/main/java/com/adriangarciao/person_productivity_app/PersonProductivityAppApplication.java
@@ -0,0 +1,13 @@
+package com.adriangarciao.person_productivity_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonProductivityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonProductivityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adriangarciao/person_productivity_app/PersonProductivityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriangarciao.person_productivity_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonProductivityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
